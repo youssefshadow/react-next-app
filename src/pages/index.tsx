87bb1059497d35ae07e9c91be838236a1071a629
youssefshadow <youssef20ben@gmail.com>
2024-02-14 18:28:30 +0100

    first component omodify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <Seo title="Coding field" description="Description..."/>
-    <h1>Hello world</h1>
+    <h1>Welcome in my app</h1>
       
     </>
   );
