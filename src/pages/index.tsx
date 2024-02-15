db8e2465a21fbf31ad03bb97fbff7ac527255bb6
youssefshadow <youssef20ben@gmail.com>
2024-02-15 18:53:11 +0100

    add button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,30 @@ export default function Home() {
     <>
         <Seo title="Coding field" description="Description..."/>
 
-        <Button>
-          click
-        </Button>
+        <div className="flex items-center gap-4 p-10">
+
+              <Button>Accent</Button>
+              <Button size="small" variant="secondary">Accent</Button>
+              <Button size="small" variant="outline">Accent</Button>
+              <Button size="small" variant="disabled" disabled>Accent</Button>
+              </div>
+        <div className="flex items-center gap-4 p-10">
+
+              <Button>Accent</Button>
+              <Button variant="secondary">Accent</Button>
+              <Button variant="outline">Accent</Button>
+              <Button variant="disabled" disabled>Accent</Button>
+        </div>
+       
+        <div className="flex items-center gap-4 p-10">
+
+            <Button>Accent</Button>
+            <Button size="large" variant="secondary">Accent</Button>
+            <Button size="large" variant="outline">Accent</Button>
+            <Button size="large" variant="disabled" disabled>Accent</Button>
+        </div>
+
+
 
 
 
--- src/ui/design-system/button/button.tsx
+++ src/ui/design-system/button/button.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 interface Props {
     size?: "small" |"medium" |"large";
-    variant?: "accent" |"secondary" |"outline" |"disable"|"ico";
+    variant?: "accent" |"secondary" |"outline" |"disabled"|"ico";
     icon?: any;
     iconTheme?:"accent" |"secondary" |"gray";
     iconPosition?:"left" |"right";
@@ -22,8 +22,48 @@ disabled,
 isLoading,
 children,
 }: Props) => {
+    let variantStyle:string = "",
+    sizeStyle:string="",
+    icoSize:number=0;
+
+    switch (variant) {
+        case "accent": //style par defaut
+            variantStyle="bg-primary hover:bg-primary-400 text-white rounded"
+            break;
+        case "secondary": 
+        variantStyle="bg-primary-200 hover:bg-primary-300/50 text-primary rounded"
+            break;
+        case "outline": 
+            variantStyle="bg-white hover:bg-gray-400/50 border border-gray-500 text-gray-900 rounded "
+            break;
+        case "disabled": 
+            variantStyle="bg-gray-400 border border-gray-500 text-gray-600 rounded cursor-not-allowed"
+            break;
+        case "ico": 
+            variantStyle=""
+            break;
+    
+        default:
+            break;
+    }
+
+    switch (size) {
+        case "small":
+            sizeStyle="text-caption3 font-medium px-[14px] py-[12px]"
+            break;
+        case "medium"://taille par défaut
+        sizeStyle="text-caption2 font-medium px-[18px] py-[15px]"
+            break;
+        case "large":
+            sizeStyle="text-caption1 font-medium px-[22px] py-[18px]"
+            break;
+        
+    
+        default:
+            break;
+    }
     return (<>
-    <button type="button"className={clsx("")} onClick={()=>console.log('Clicked Button')}disabled={disabled}>
+    <button type="button"className={clsx(variantStyle,icoSize,sizeStyle,"")} onClick={()=>console.log('Clicked Button')}disabled={disabled}>
         {children}
     </button>
     
