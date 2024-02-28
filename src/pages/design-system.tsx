d058de25b16ef3af8cfb85f71d8a6507969fe3c2
youssefshadow <youssef20ben@gmail.com>
2024-02-28 18:40:50 +0100

    navigation link

---
+++ src/pages/design-system.tsx
@@ -0,0 +1,131 @@
+import { Container } from "@/ui/components/container/container";
+import { Navigation } from "@/ui/components/navigation/navigation";
+import { Seo } from "@/ui/components/seo/seo";
+import { Avatar } from "@/ui/design-system/avatar/avatar";
+import { Button } from "@/ui/design-system/button/button";
+import { Logo } from "@/ui/design-system/logo/logo";
+import { Spinner } from "@/ui/design-system/spinner/spinner";
+import { Typography } from "@/ui/design-system/typoghraphy/typography";
+import { RiUser6Fill } from "react-icons/ri";
+
+export default function DesignSystem(){
+    return(
+        <>
+        <Seo title="Coding jungle" description="Description..." />
+        <Navigation />
+  
+       <Container className="py-10 space-y-10 text-center">
+          {/* Section logo */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded mx-auto">
+            <Typography variant="caption2" weight="medium">Logo</Typography>
+            <Logo size="very-small"/>
+            <Logo size="small"/>
+            <Logo/>
+            <Logo size="large"/>
+          </div>
+  
+          {/* Section Avatar */}
+          <div className="space-y-2">
+            <Typography variant="caption2" weight="medium">Avatar</Typography>
+            <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+              <Avatar size="small" src="/assets/images/avatar.webp" alt="avatar bruce wayne"/>
+              <Avatar src="/assets/images/avatar.webp" alt="avatar bruce wayne"/>
+              <Avatar size="large" src="/assets/images/avata2.avif" alt="avatar bruce wayne"/>
+              <Avatar size="large" src="/assets/images/dragon.webp" alt="avatar bruce wayne"/>
+            </div>
+          </div>
+  
+          {/* Section 1 */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Spinner size="small"></Spinner>
+            <Spinner size="medium"></Spinner>
+            <Spinner size="large"></Spinner>
+          </div>
+  
+          {/* Section 2 */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Button isLoading size="small" icon={{ icon: RiUser6Fill }} iconPosition="left">
+              Accent
+            </Button>
+            <Button isLoading size="small">
+              Accent
+            </Button>
+            <Button isLoading size="small" variant="outline">
+              Accent
+            </Button>
+            <Button isLoading size="small" variant="disabled" disabled>
+              Accent
+            </Button>
+            <Button isLoading size="small" variant="ico" icon={{ icon: RiUser6Fill }} />
+          </div>
+  
+          {/* Section 3 */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Button size="small" icon={{ icon: RiUser6Fill }} iconPosition="left">
+              Accent
+            </Button>
+            <Button size="small" variant="secondary">
+              Accent
+            </Button>
+            <Button size="small" variant="outline">
+              Accent
+            </Button>
+            <Button size="small" variant="disabled" disabled>
+              Accent
+            </Button>
+            <Button size="small" variant="ico" icon={{ icon: RiUser6Fill }} />
+          </div>
+  
+          {/* Section 4 */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Button>Accent</Button>
+            <Button variant="secondary">Accent</Button>
+            <Button variant="outline">Accent</Button>
+            <Button variant="disabled" disabled>
+              Accent
+            </Button>
+            <Button variant="ico" icon={{ icon: RiUser6Fill }} />
+          </div>
+  
+          {/* Section 5 */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Button>Accent</Button>
+            <Button size="large" variant="secondary">
+              Accent
+            </Button>
+            <Button size="large" variant="outline">
+              Accent
+            </Button>
+            <Button size="large" variant="disabled" disabled>
+              Accent
+            </Button>
+            <Button size="large" variant="ico" icon={{ icon: RiUser6Fill }} />
+            <Button size="large" variant="ico" iconTheme="secondary" icon={{ icon: RiUser6Fill }} />
+            <Button size="large" variant="ico" iconTheme="gray" icon={{ icon: RiUser6Fill }} />
+          </div>
+  
+          {/* Section Typography */}
+          <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+            <Typography theme="black" variant="h1" component="div">
+              Aventure dans le monde du code
+            </Typography>
+            <Typography theme="secondary" variant="h1" component="div">
+              Découvertes infinies en programmation
+            </Typography>
+            <Typography variant="lead" component="div">
+              Explorez les mystères du développement logiciel
+            </Typography>
+            <Typography variant="body-sm" component="div">
+              Codez avec passion, créez avec ingéniosité
+            </Typography>
+            <Typography variant="caption4" component="div">
+              Cet univers du code vous attend, prêt à être exploré
+            </Typography>
+            <Typography variant="caption4" weight="medium" component="div">
+              Écrivez votre propre histoire dans le langage binaire de la créativité
+            </Typography>
+          </div>
+        </Container>
+        </>
+    )
+}
--- src/ui/components/navigation/navigation.tsx
+++ src/ui/components/navigation/navigation.tsx
@@ -2,27 +2,30 @@ import { Logo } from "@/ui/design-system/logo/logo";
 import { Container } from "../container/container";
 import { Typography } from "@/ui/design-system/typoghraphy/typography";
 import { Button } from "@/ui/design-system/button/button";
+import Link from "next/link";
 
 interface Props {}
 
 export const Navigation = ({}: Props) => {
   return (
-    <div className=" w-full border-b-2 border-gray-400 py-4">
+    <div className=" w-full border-b-2 border-gray-400 ">
       <Container className="flex items-center justify-between py-1.5 gap-7">
-        <div className="flex items-center gap-2.5">
-          <Logo size="small" />
-          <div className="flex flex-col">
-            <div className="text-gray font-extrabold text-[24px]">
-              Coding Jungle
+        <Link href="/">
+            <div className="flex items-center gap-2.5">
+              <Logo size="small" />
+              <div className="flex flex-col">
+                <div className="text-gray font-extrabold text-[24px]">
+                  Coding Jungle
+                </div>
+                <Typography variant="caption4" theme="gray" component="span">
+                  Plonge dans l&apos;univers de l&apos;inspiration tout en recevant des feedbacks
+                </Typography>
+              </div>
             </div>
-            <Typography variant="caption4" theme="gray" component="span">
-              Plonge dans l&apos;univers de l&apos;inspiration tout en recevant des feedbacks
-            </Typography>
-          </div>
-        </div>
+        </Link>
         <div className="flex items-center gap-7">
           <Typography variant="caption3" component="div" className="flex items-center gap-7">
-            <span>Projets</span>
+            <Link href="/design-system">Design system</Link>
             <span>Projets</span>
             <span>Projets</span>
           </Typography>
