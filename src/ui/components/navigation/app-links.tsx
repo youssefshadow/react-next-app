1daca6ae41b656a42cff241697df8ca300b9ae8b
youssefshadow <youssef20ben@gmail.com>
2024-03-03 18:41:11 +0100

    applink file

---
+++ src/ui/components/navigation/app-links.tsx
@@ -0,0 +1,22 @@
+const footerApplicationLinks = [
+  {
+    label: "Accueil",
+    baseUrl: "/",
+    type: "internal",
+  },
+  {
+    label: "Projets",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Coding jungle",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Formations",
+    baseUrl: "https://www.youtube.com/",
+    type: "external",
+  },
+];
