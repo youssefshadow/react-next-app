1842df595d0104fe8595a64a0043e28365f614b5
youssefshadow <youssef20ben@gmail.com>
2024-03-03 18:50:59 +0100

    applink data tabs

--- src/ui/components/navigation/app-links.tsx
+++ src/ui/components/navigation/app-links.tsx
@@ -1,4 +1,6 @@
-const footerApplicationLinks = [
+import { AppLinks } from "@/types/app-links";
+
+const footerApplicationLinks: AppLinks[] = [
   {
     label: "Accueil",
     baseUrl: "/",
@@ -17,6 +19,70 @@ const footerApplicationLinks = [
   {
     label: "Formations",
     baseUrl: "https://www.youtube.com/",
+    type: "external",
+  },
+];
+
+const footerUserLinks: AppLinks[] = [
+  {
+    label: "Mon espace",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Connexion",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Inscription",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Mot de passe oublié",
+    baseUrl: "/#",
+    type: "internal",
+  },
+];
+
+const footerInformationsLinks: AppLinks[] = [
+  {
+    label: "CGU",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Confidentialité",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "A propos",
+    baseUrl: "/#",
+    type: "internal",
+  },
+  {
+    label: "Contact",
+    baseUrl: "/#",
+    type: "internal",
+  },
+];
+
+const footerSocialNetworksLinks: AppLinks[] = [
+  {
+    label: "Youtube",
+    baseUrl: "https://www.youtube.com/",
+    type: "external",
+  },
+  {
+    label: "Linkedin",
+    baseUrl: "https://www.linkedin.com/in/youssef-benslimane-264939228/",
+    type: "external",
+  },
+  {
+    label: "Github",
+    baseUrl: "https://github.com/youssefshadow",
     type: "external",
   },
 ];
