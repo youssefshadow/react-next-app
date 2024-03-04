c5e70bd50319f3ab184bad6300af0116d3fc8b45
youssefshadow <youssef20ben@gmail.com>
2024-03-04 20:57:46 +0100

    social network icon

--- src/ui/components/navigation/social-networks-buttons.tsx
+++ src/ui/components/navigation/social-networks-buttons.tsx
@@ -3,8 +3,13 @@ import { Button } from "@/ui/design-system/button/button";
 import { footerSocialNetworksLinks } from "./app-links";
 import { v4 as uuidv4 } from "uuid";
 import { RiLinkedinBoxFill } from "react-icons/ri";
+import clsx from "clsx";
 
-export const SocialNetworksButtons = () => {
+interface Props {
+  className?: string;
+}
+
+export const SocialNetworksButtons = ({ className }: Props) => {
   const icoList = footerSocialNetworksLinks.map((socialNetwork) => (
     <Button
       key={uuidv4()}
@@ -14,5 +19,9 @@ export const SocialNetworksButtons = () => {
     />
   ));
 
-  return <div>{icoList}</div>;
+  return (
+    <div className={clsx((className = "flex item-center gap-2.5"))}>
+      {icoList}
+    </div>
+  );
 };
