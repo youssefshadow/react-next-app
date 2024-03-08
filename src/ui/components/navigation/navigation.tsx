69e5725ed462087ae2738455d0318a6c2663da4a
youssefshadow <youssef20ben@gmail.com>
2024-03-08 10:20:04 +0100

    tailwind gray modified

--- src/ui/components/navigation/navigation.tsx
+++ src/ui/components/navigation/navigation.tsx
@@ -9,23 +9,28 @@ interface Props {}
 
 export const Navigation = ({}: Props) => {
   return (
-    <div className=" w-full border-b-2 border-gray-400 ">
+    <div className="w-full border-b-2 border-gray-400 p-2.5">
       <Container className="flex items-center justify-between py-1.5 gap-7">
         <Link href="/">
-            <div className="flex items-center gap-2.5">
-              <Logo size="small" />
-              <div className="flex flex-col">
-                <div className="text-gray font-extrabold text-[24px]">
-                  Coding Jungle
-                </div>
-                <Typography variant="caption4" theme="gray" component="span">
-                  Plonge dans l&apos;univers de l&apos;inspiration tout en recevant des feedbacks
-                </Typography>
+          <div className="flex items-center gap-2.5">
+            <Logo size="small" />
+            <div className="flex flex-col">
+              <div className="text-gray font-extrabold text-[24px]">
+                Coding Jungle
               </div>
+              <Typography variant="caption4" theme="gray" component="span">
+                Plonge dans l&apos;univers de l&apos;inspiration tout en
+                recevant des feedbacks
+              </Typography>
             </div>
+          </div>
         </Link>
         <div className="flex items-center gap-7">
-          <Typography variant="caption3" component="div" className="flex items-center gap-7">
+          <Typography
+            variant="caption3"
+            component="div"
+            className="flex items-center gap-7"
+          >
             <ActiveLink href="/design-system">Design system</ActiveLink>
             <Link href="projets">Projets</Link>
             <Link href="contacts">Contacts</Link>
@@ -33,12 +38,10 @@ export const Navigation = ({}: Props) => {
           </Typography>
           <div className="flex items-center gap-2">
             <Button size="small">Connexion</Button>
-            <Button size="small" variant="secondary">Rejoindre</Button>
-
-
+            <Button size="small" variant="secondary">
+              Rejoindre
+            </Button>
           </div>
-          
-
         </div>
       </Container>
     </div>
--- src/ui/modules/landing-page/components/featured/featured.view.tsx
+++ src/ui/modules/landing-page/components/featured/featured.view.tsx
@@ -1,3 +1,3 @@
 export const Featured = () => {
-  return <div className="bg-gray-400 h-96 w-96"></div>;
+  return <div className="bg-gray-300 h-96 w-96"></div>;
 };
